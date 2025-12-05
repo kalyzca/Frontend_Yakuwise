350bac1972034de075bcb6e050c4d4536e11718a
kalicita <alcantarakaly@gmail.com>
2025-12-04 23:18:03 -0500

    [ADD] Estructura del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './features/welcome/welcome-component/welcome-component';
+import { LoginComponent } from './features/login/login-component/login-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'**',
+    redirectTo:''
+  }
+
+];
